038563706ce1a3137ebb470efa17bacca47ae975
labib108 <abubuckkersiddikh@gmail.com>
2023-02-18 11:43:21 +0600

    factoroial

--- D1/factorial.php
+++ D1/factorial.php
@@ -11,3 +11,17 @@
 9! = 362880
 using recursive function to get Factorial of a number
 */
+<?php
+    
+    function factorial($n)
+    {
+        if($n == 0)
+            return 1;
+        else
+            return ($n * factorial($n - 1));
+    }
+    for($i = 0; $i < 10; $i++)
+    {
+        echo "Factorial of " . $i . " is " . factorial($i) . "\n";
+    }
+?>
