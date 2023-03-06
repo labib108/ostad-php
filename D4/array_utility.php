b8bcde360a700efae6bca46d6faaf6f6ba8c5bae
Labib <abubuckkersiddikh@gmail.com>
2023-03-07 02:11:26 +0600

    array

--- D4/array_utility.php
+++ D4/array_utility.php
@@ -1,16 +1,16 @@
 <?php
 $number = [1,2,3,4,5,6,7,8,9,10];
 
-//function squre($num){
-//    printf("Squre if %d is %d\n",$num,$num*$num);
-//}
-//function cube($num){
-//    return $num*$num*$num;
-//}
-//print_r($number);
-//array_walk($number,'squre');
-//$new_number = array_map('cube',$number);
-//print_r($new_number);
+function squre($num){
+   printf("Squre if %d is %d\n",$num,$num*$num);
+}
+function cube($num){
+   return $num*$num*$num;
+}
+print_r($number);
+array_walk($number,'squre');
+$new_number = array_map('cube',$number);
+print_r($new_number);
 
 function even($num){
     if($num%2 == 0)
@@ -28,3 +28,16 @@ $even_number = array_filter($number,'even');
 $odd_number = array_filter($number,'odd');
 print_r($even_number);
 print_r($odd_number);
+
+function sum($oldValue, $newValue){
+    return $oldValue + $newValue;
+}
+$sum = array_reduce($number, 'sum');
+echo $sum;
+
+$marks = array(85,75,65,55,45,35,25,15,5);
+list($sami,$doni,$pkd,$sumon,$labib) = $marks;
+
+echo $labib;
+
+?>
